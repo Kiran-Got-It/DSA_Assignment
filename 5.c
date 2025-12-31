a470a6a35a34b00331e112a562bea833d011e857
Kiran___ <kiran.chaps2@gmail.com>
2025-12-31 15:53:27 +0545

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+ 
+int factorial(int n) { 
+     
+    if (n == 0 || n == 1) { 
+        return 1; 
+    } else { 
+ 
+              return n * factorial(n - 1); 
+    } 
+} 
+ 
+int main() { 
+    int num; 
+   int result; 
+ 
+    printf("Enter a positive integer: "); 
+    scanf("%d", &num); 
+        
+        result = factorial(num); 
+        printf("Factorial of %d is %d\n", num, result); 
+ 
+    return 0; 
+} 
